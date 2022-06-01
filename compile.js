a2aeead4c9a5f13265190898429532fbf6fba789
hengvisoth <hengratanakvisoth20@kit.edu.kh>
2022-06-01 17:39:47 +0700

    Initial Commit

--- compile.js
+++ compile.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const solc = require("solc");
 
 const inboxPath = path.resolve(__dirname, "contracts", "cryptoKid.sol");
-console.log(inboxPath); // dirname == working directory
 const source = fs.readFileSync(inboxPath, "utf8");
 
 module.exports = solc.compile(source, 1).contracts[":cryptoKid"];
